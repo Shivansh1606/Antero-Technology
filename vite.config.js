450326c8d5ad084a935044469f3a5b50a8b65764
Shivansh <shivanshsingh255.>
2026-01-02 22:37:09 +0530

    fix: change minifier to esbuild

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,7 +10,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
+    minify: 'esbuild', // terser ki jagah esbuild use karo
     rollupOptions: {
       output: {
         manualChunks: {
